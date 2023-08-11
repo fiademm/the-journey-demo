33b75652373ed2223b1ac348285539a7490c79dd
fiademm <fiademm1@gmail.com>
2023-08-11 02:16:04 +0000

    Initial project setup

---
+++ src/app/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Styles.css';
+
+const Home = () => {
+    return(
+        <>
+            <div className="component-container">
+                <span className='subtext'>THE PAGE IS NOT READY YET</span>
+                <hr className="hline line1" />
+                <span className="name">FIADEMM</span>
+                <span className='subtext'>LET'S BUILD A BRIGTHER TECH FUTURE TOGETHER</span>
+                <hr className="hline line2" />
+            </div>
+        </>
+    );
+}
+
+export default Home;
